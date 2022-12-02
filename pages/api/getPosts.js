c3c8551b2ec7c2c4da107f29d97dd24fcc69d99e
Elias Souza <eliasnsouza1245@gmail.com>
2022-12-02 01:07:56 -0300

    Update getPosts.js

--- pages/api/getPosts.js
+++ pages/api/getPosts.js
@@ -24,8 +24,35 @@ export default async function handler(req, res) {
   if (req.method === "GET") {
 
     const posts = await postlist.find().toArray()
-    return res.status(200).send(JSON.parse(posts))
+    return res.status(200).send(posts)
   
   }
 
+
+  if (req.method === "POST") {
+
+    const { title, body, author} = req.body
+
+    const newPost = {
+      title,
+      body,
+      author,
+      date: new Date()
+    }
+
+    await postlist.insertOne(newPost)
+    return res.status(200).send("Sucess")
+  
+  }
+
+
+  if (req.method === "DELETE") {
+  
+    const { uuid } = req.body
+    const deleted = await postlist.findOneAndDelete({ _id: ObjectId(uuid) })
+    return res.status(200).send("Deleted")
+
+  }
+  
+  return res.status(200).send("ok")
 }
